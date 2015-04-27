4a119b120089672d81a280ac03722a8606631fd7
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-04-27 23:44:30 +0900

    Add zip, zip3, zipWith, zipWith3 and unzip3

--- src/List/Extra.elm
+++ src/List/Extra.elm
@@ -4,6 +4,9 @@ module List.Extra (minimumBy, maximumBy) where
 # Common Helpers
 @docs maximumBy, minimumBy
 
+# Zipping
+@docs zip, zip3, zipWith, zipWith3, unzip3
+
 -}
 
 import List exposing (..)
@@ -25,3 +28,44 @@ minimumBy f ls =
   in case ls of
         l'::ls' -> Just <| foldl (minBy f) l' ls'
         _       -> Nothing
+
+{-| Take two lists and returns a list of corresponding pairs
+-}
+zip : List a -> List b -> List (a,b)
+zip xs ys =
+  case (xs, ys) of
+    (x::xs', y::ys') -> (x,y) :: zip xs' ys'
+    (_     , _     ) -> []
+
+{-| Take three lists and returns a list of triples
+-}
+zip3 : List a -> List b -> List c -> List (a,b,c)
+zip3 xs ys zs =
+  case (xs, ys, zs) of
+    (x::xs', y::ys', z::zs') -> (x,y,z) :: zip3 xs' ys' zs'
+    (_     , _     , _     ) -> []
+
+{-| Generalise zip by zipping with the function given as the first argument
+-}
+zipWith : (a -> b -> c) -> List a -> List b -> List c
+zipWith f xs ys =
+  case (xs, ys) of
+    (x::xs', y::ys') -> f x y :: zipWith f xs' ys'
+    (_     , _     ) -> []
+
+{-| Take a function which combines three elements, as well as three lists and returns a list of their point-wise combination
+-}
+zipWith3 : (a -> b -> c -> d) -> List a -> List b -> List c -> List d
+zipWith3 f xs ys zs =
+  case (xs, ys, zs) of
+    (x::xs', y::ys', z::zs') -> f x y z :: zipWith3 f xs' ys' zs'
+    (_     , _     , _     ) -> []
+
+{-| Take a list of triples and returns three lists
+-}
+unzip3 : List (a,b,c) -> (List a, List b, List c)
+unzip3 triples =
+  let step (x,y,z) (xs,ys,zs) =
+    (x::xs, y::ys, z::zs)
+  in
+    foldr step ([], [], []) triples
