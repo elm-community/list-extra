d8bd698912db4bc23958771442ab57dfdad97888
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-04-29 11:56:59 +0900

    Use mapN

--- src/List/Extra.elm
+++ src/List/Extra.elm
@@ -32,16 +32,10 @@ minimumBy f ls =
 {-| Take two lists and returns a list of corresponding pairs
 -}
 zip : List a -> List b -> List (a,b)
-zip xs ys =
-  case (xs, ys) of
-    (x::xs', y::ys') -> (x,y) :: zip xs' ys'
-    (_     , _     ) -> []
+zip = map2 (,)
 
 {-| Take three lists and returns a list of triples
 -}
 zip3 : List a -> List b -> List c -> List (a,b,c)
-zip3 xs ys zs =
-  case (xs, ys, zs) of
-    (x::xs', y::ys', z::zs') -> (x,y,z) :: zip3 xs' ys' zs'
-    (_     , _     , _     ) -> []
+zip3 = map3 (,,)
 
