fa06ccd04b7649b684386a16ccae76c1fb760c37
Rehno Lindeque <errantkid@gmail.com>
2015-07-21 15:16:34 -0700

    Slightly improved documentation formatting

--- src/List/Extra.elm
+++ src/List/Extra.elm
@@ -72,6 +72,7 @@ andMap fl l = map2 (<|) fl l
 
 {-| Find the first element that satisfies a predicate and return
 Just that element. If none match, return Nothing.
+
     find (\num -> num > 5) [2, 4, 6, 8] == Just 6
 -}
 find : (a -> Bool) -> List a -> Maybe a
