elm-community/list-extra

commit 5fa63ca848bec2da957282ae62481caa00c37f9c

Optimize List.Extra#init
This one looks a little funky, but counterintuituvely, it's a bunch faster for
nonempty lists.

`foldr` needs two passes, too. The big win here is that it's extremely easy to
comprehend: flip it, chop off the head, flip it again.

As for why it's faster: as stated, foldr needs two passes. First, it needs to go
to the end of the list, then back to the beginning while applying a function to
every element. In this implementation, we need two passes, too, but we only
apply a single, constant time function between the passes.

https://ellie-app.com/fFq6s2PcTa1/0 for benchmarks.

Long story short, 70% slower for empty lists (since we still need to call 3
functions), 50% faster for a single element, 300 to 500% improvement for lists
of 10 to 1000 elements.

src/List/Extra.elm
@@ -161,13 +161,11 @@ last =
 
 -}
 init : List a -> Maybe (List a)
-init =
-    let
-        maybe : b -> (List a -> b) -> Maybe (List a) -> b
-        maybe d f =
-            Maybe.withDefault d << Maybe.map f
-    in
-        foldr (\x -> maybe [] ((::) x) >> Just) Nothing
+init items =
+    items
+        |> List.reverse
+        |> List.tail
+        |> Maybe.map List.reverse
 
 
 {-| Returns `Just` the element at the given index in the list,

commit cbdc9a6ccda79bd53ee30b9962a52da9208d65f0

Fix performance regression in List.Extra#init
Commit 5fa63ca introduced a performance improvement for non empty lists
but also a ~70% performance regression for empty lists. This commit
fixes the regression while also improving the performance ~30% for
empty lists before 5fa63ca.

https://ellie-app.com/5K7f2nGwsa1/1 for benchmarks.

src/List/Extra.elm
@@ -170,10 +170,15 @@ last items =
 -}
 init : List a -> Maybe (List a)
 init items =
-    items
-        |> List.reverse
-        |> List.tail
-        |> Maybe.map List.reverse
+    case items of
+        [] ->
+            Nothing
+
+        nonEmptyList ->
+            nonEmptyList
+                |> List.reverse
+                |> List.tail
+                |> Maybe.map List.reverse
 
 
 {-| Returns `Just` the element at the given index in the list,